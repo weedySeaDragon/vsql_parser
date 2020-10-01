a876743a68aae180616355f44d106aba6206625e
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2020-10-01 07:26:33 -0700

    orig 'test chamber' file

--- lib/test_chamber.rb
+++ lib/test_chamber.rb
@@ -2,15 +2,18 @@
 require_relative './psql_parser.rb'
 
 module TestChamber
-  module Helpers
-    NORMAL_COLOR ||= 37
-    def colorize(color, output)
-      "\e[0;#{color}m#{output}\e[0;#{NORMAL_COLOR}m"
-    end
+
+
+  NORMAL_COLOR ||= 37
+
+
+  def colorize(color, output)
+    "\e[0;#{color}m#{output}\e[0;#{NORMAL_COLOR}m"
   end
 
-  include Helpers
+
   extend self
+
 
   def parse(sql, output_errors = true)
     parser = ::PSqlParser.parser
@@ -24,25 +27,28 @@ module TestChamber
     end
   end
 
+
   private
 
   def output_error(sql, parser)
     fail_index = parser.max_terminal_failure_index
     STDERR.flush
     STDOUT.flush
-    STDERR.puts( "\n" +
-                 ((fail_index > 0) ? colorize(42, sql[0..(fail_index - 1)]) : "") +
-                 colorize(41, sql[(fail_index)..-1]) +
-                 "\n\n")
+    STDERR.puts("\n" +
+                  ((fail_index > 0) ? colorize(42, sql[0..(fail_index - 1)]) : "") +
+                  colorize(41, sql[(fail_index)..-1]) +
+                  "\n\n")
 
     STDERR.flush
   end
 
+
   def clean_tree(root_node)
-    return if(root_node.elements.nil?)
-    root_node.elements.delete_if{|node| node.class.name == "Treetop::Runtime::SyntaxNode" }
-    root_node.elements.each {|node| self.clean_tree(node) }
+    return if (root_node.elements.nil?)
+    root_node.elements.delete_if { |node| node.class.name == "Treetop::Runtime::SyntaxNode" }
+    root_node.elements.each { |node| self.clean_tree(node) }
   end
+
 
   def reload
     Object.send(:remove_const, :SqlParser) rescue nil
